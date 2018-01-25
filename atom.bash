622a83483f689c1ce5076d1fc62ef8674ae60e21
Hirofumi Wakasugi <baenej@gmail.com>
2018-01-25 14:12:24 +0900

    Add VSCode

--- atom.bash
+++ atom.bash
@@ -3,26 +3,26 @@
 # https://discuss.atom.io/t/how-to-completely-uninstall-atom-for-mac/9084/42
 
 files_to_delete=(
-  "~/.atom"
+  "${HOME}/.atom"
   "/usr/local/bin/atom"
   "/usr/local/bin/apm"
   "/Applications/Atom.app"
-  "~/Library/Preferences/com.github.atom.plist"
-  "~/Library/Application Support/com.github.atom.ShipIt"
-  "~/Library/Application Support/Atom"
-  "~/Library/Saved Application State/com.github.atom.savedState"
-  "~/Library/Caches/com.github.atom"
-  "~/Library/Caches/com.github.atom.ShipIt"
-  "~/Library/Caches/Atom"
+  "${HOME}/Library/Preferences/com.github.atom.plist"
+  "${HOME}/Library/Application Support/com.github.atom.ShipIt"
+  "${HOME}/Library/Application Support/Atom"
+  "${HOME}/Library/Saved Application State/com.github.atom.savedState"
+  "${HOME}/Library/Caches/com.github.atom"
+  "${HOME}/Library/Caches/com.github.atom.ShipIt"
+  "${HOME}/Library/Caches/Atom"
 )
 
 for e in "${files_to_delete[@]}"; do
     if [ -d "${e}" ]; then
         echo "Removing existing directory: [${e}] ..."
-	mv "${e}" ~/.Trash/
+	mv "${e}" "${HOME}/.Trash/"
     elif [ -f "${e}" ]; then
         echo "Removing existing file: [${e}] ..."
-	mv "${e}" ~/.Trash/
+	mv "${e}" "${HOME}/.Trash/"
     else
         echo "[${e}] does not exist."
     fi
